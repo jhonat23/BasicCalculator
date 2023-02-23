69e970b30d2b617c3a61492784fab449a0e69ae8
jhonat23 <jhonat23@gmail.com>
2023-02-23 17:30:10 -0500

    Add project files.

---
+++ BasicCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(
+            "Hey! Welcome to the calculator where you can solve your daily operations\n" + 
+            "Please, select the required operation\n" + 
+            "1 - Sum\n" + 
+            "2 - Substraction\n" + 
+            "3 - Multiplication\n" + 
+            "4 - Division\n"+
+            "\n" +
+            "Or you can exit pressing 'e' on your keyboard\n"
+            );
+
+        
+    }
+}
